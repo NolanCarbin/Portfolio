456697cc54afd66271871c5beeb590514d7627ab
Nolan Carbin <nolan2556@gmail.com>
2021-09-15 19:12:33 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav>
+      <router-link :to="{name: 'Home'}" exact>Home</router-link>
+      <router-link :to="{name: 'Projects'}">Projects</router-link>
+      <router-link :to="{name: 'Contact'}">Contact</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
